b29a4c0bafba48b963a7efd44386403d2c98d9d9
Trolorol <joao.calapez.c@gmail.com>
2020-12-01 21:52:09 +0000

    Added for loop function to studenGrade.js

--- public/js/studentGrades.js
+++ public/js/studentGrades.js
@@ -4,29 +4,42 @@ var secondunitName = "Literature"
 var firstUnitGrade = 7.5
 var secondUnitGrade = 11.2
 
+var units = [firstUnitName, secondunitName]
+var grades = [firstUnitGrade, secondUnitGrade]
+
+
 window.onload = function () {
+    var i;
     document.getElementById("student").innerHTML = name;
-// TODO: Fazer o segundo cartão, são funções repetidas?
     let card = ""
-    card += "<div id='grade-card'>"
-    card += "<div id='grade-card-substring'><h1>" + firstUnitName.substring(0,2) + "</h1></div>"
-    card += "<div id='unit-data'><h2>" + firstUnitName + "</h2>" + "<h4> Grade: " + firstUnitGrade + "</h4>";
-    card += "</div>"
-    document.getElementById("grade-card-line").innerHTML = card;
+
+    for (i = 0; i < units.length; i++){
+        card += "<div class='grade-card'>"
+        card += "<div class='grade-card-substring'><h1>" + units[i].substring(0,2) + "</h1></div>"
+        card += "<div class='unit-data'><h2>" + units[i] + "</h2>" + "<h4> Grade: " + grades[i] + "</h4>";
+        card += "</div> </div>";
+        console.log(card)
+        
+        document.getElementById("grade-card-line").innerHTML = card
+
+    }
+    
 }
 
 
-/* <div id="grade-card-line">
 
-<div id="grade-card">
+// var i;
+// for (i = 0; i < cars.length; i++) {
+//   text += cars[i] + "<br>";
+// }
 
-    <div id="grade-card-substring"><h1>Ma</h1></div>
 
-    <div id="unit-data">
-        <h2>Mathemathics</h2>
-        <h3>7,5</h3>
-    </div>
-
-</div>
-
-</div> */
+// window.onload = function () {
+//     document.getElementById("student").innerHTML = name;
+//     let card = ""
+//     card += "<div class='grade-card'>"
+//     card += "<div class='grade-card-substring'><h1>" + firstUnitName.substring(0,2) + "</h1></div>"
+//     card += "<div class='unit-data'><h2>" + firstUnitName + "</h2>" + "<h4> Grade: " + firstUnitGrade + "</h4>";
+//     card += "</div> </div>";
+//     document.getElementById("grade-card-line").innerHTML = card + card
+// }
